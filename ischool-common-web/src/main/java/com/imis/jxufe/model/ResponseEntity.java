ed23369ca72e7db0fb3cae3c3de509c7f2f6e084
zhongpingtang <you@example.com>
2017-03-16 22:11:35 +0800

    create  ResponseEntity

---
+++ ischool-common-web/src/main/java/com/imis/jxufe/model/ResponseEntity.java
@@ -0,0 +1,62 @@
+package com.imis.jxufe.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhongping
+ * @date 2017/3/16
+ */
+public class ResponseEntity implements Serializable {
+
+    private static final long serialVersionUID = -1483013122646260435L;
+
+    /**
+     * 响应状态
+     */
+    private int resStatus;
+
+    /**
+     * 响应信息
+     */
+    private String resMsg;
+
+    /**
+     * 响应参数
+     */
+    private Map<String, Object> params = new HashMap<String, Object>();
+
+    public ResponseEntity() {
+    }
+
+    public ResponseEntity(int resStatus, String resMsg) {
+        super();
+        this.resStatus = resStatus;
+        this.resMsg = resMsg;
+    }
+
+    public int getResStatus() {
+        return resStatus;
+    }
+
+    public void setResStatus(int resStatus) {
+        this.resStatus = resStatus;
+    }
+
+    public String getResMsg() {
+        return resMsg;
+    }
+
+    public void setResMsg(String resMsg) {
+        this.resMsg = resMsg;
+    }
+
+    public Map<String, Object> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, Object> params) {
+        this.params = params;
+    }
+}
