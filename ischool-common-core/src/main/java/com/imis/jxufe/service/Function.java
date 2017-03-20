5671b3744fc1124a100ff4d4a6f99488b37c4fa8
Penn <penn2017@163.com>
2017-03-21 01:12:02 +0800

    创建redis等

---
+++ ischool-common-core/src/main/java/com/imis/jxufe/service/Function.java
@@ -0,0 +1,7 @@
+package com.imis.jxufe.service;
+
+public interface Function<T, E> {
+
+    public T callback(E e);
+
+}
---
+++ ischool-common-core/src/test/java/com/imis/jxufe/utils/SimpleWebUtilTest.java
@@ -0,0 +1,9 @@
+package com.imis.jxufe.utils;
+
+/**
+ * @author zhongping
+ * @date 2017/3/17
+ */
+public class SimpleWebUtilTest {
+
+}
