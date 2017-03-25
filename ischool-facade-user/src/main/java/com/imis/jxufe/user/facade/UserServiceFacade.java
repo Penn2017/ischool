fc95fd3a82834eca15a44507e4f2fd7fae9d688a
zhongpingtang <you@example.com>
2017-03-25 15:53:06 +0800

    修改mail服务

--- ischool-facade-user/src/main/java/com/imis/jxufe/user/facade/UserServiceFacade.java
+++ ischool-facade-user/src/main/java/com/imis/jxufe/user/facade/UserServiceFacade.java
@@ -7,6 +7,23 @@ package com.imis.jxufe.user.facade;
 public interface UserServiceFacade {
 
 
+    /**
+     * 检查用户的邮箱是否存在
+     * @param email
+     * @return
+     */
+    boolean userEmailIsExist(String email);
+
+    /**
+     * 创建一个用户
+     * @param email
+     * @param name
+     * @param passwd
+     * @return
+     */
+    boolean createUser(String email,String name,String passwd);
+
+
 
 
 
--- ischool-service-user/src/main/java/com/imis/jxufe/user/facade/service/impl/UserServiceFacadeImpl.java
+++ ischool-service-user/src/main/java/com/imis/jxufe/user/facade/service/impl/UserServiceFacadeImpl.java
@@ -2,6 +2,7 @@ package com.imis.jxufe.user.facade.service.impl;
 
 import com.imis.jxufe.user.facade.UserServiceFacade;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 对外暴露的用户服务接口
@@ -10,6 +11,20 @@ import org.springframework.stereotype.Component;
  * @date 2017/3/21
  */
 @Component("userService")
+@Transactional
 public class UserServiceFacadeImpl implements UserServiceFacade {
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public boolean userEmailIsExist(String email) {
+        return false;
+    }
+
+
+    @Override
+    public boolean createUser(String email, String name, String passwd) {
+        return false;
+    }
 
 }
