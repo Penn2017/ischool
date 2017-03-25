378f15988468f8f6dde2987836b07b02e38084aa
zhongpingtang <you@example.com>
2017-03-25 14:05:25 +0800

    相安无事版本

--- ischool-facade-user/src/main/java/com/imis/jxufe/user/facade/UserServiceFacade.java
+++ ischool-facade-user/src/main/java/com/imis/jxufe/user/facade/UserServiceFacade.java
@@ -1,12 +1,5 @@
 package com.imis.jxufe.user.facade;
 
-import com.imis.jxufe.model.IschoolUser;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.BindingResult;
-
-import javax.validation.Valid;
-
 /**
  * @author zhongping
  * @date 2017/3/21
@@ -14,22 +7,7 @@ import javax.validation.Valid;
 public interface UserServiceFacade {
 
 
-    /**
-     * 根据用户的userKey判断用户的key是否存在
-     * @param userKey
-     * @return
-     */
-    @Transactional(readOnly = true,propagation = Propagation.REQUIRED)
-    boolean userIsExist(String userKey);
 
-
-    /**
-     * 创建用户
-     * @param unStoreUser
-     * @return
-     */
-    @Transactional
-    IschoolUser  createUser(@Valid IschoolUser unStoreUser, BindingResult errorResult);
 
 
 
--- ischool-service-user/src/main/java/com/imis/jxufe/user/facade/service/impl/UserServiceFacadeImpl.java
+++ ischool-service-user/src/main/java/com/imis/jxufe/user/facade/service/impl/UserServiceFacadeImpl.java
@@ -1,14 +1,7 @@
 package com.imis.jxufe.user.facade.service.impl;
 
-import com.imis.jxufe.core.service.RedisService;
-import com.imis.jxufe.model.IschoolUser;
 import com.imis.jxufe.user.facade.UserServiceFacade;
-import com.imis.jxufe.user.mapper.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.BindingResult;
-
-import javax.validation.Valid;
 
 /**
  * 对外暴露的用户服务接口
@@ -19,27 +12,4 @@ import javax.validation.Valid;
 @Component("userService")
 public class UserServiceFacadeImpl implements UserServiceFacade {
 
-    @Autowired
-    private UserMapper userDao;
-
-    @Autowired
-    private RedisService redis;
-
-    @Override
-    public boolean userIsExist(String userKey) {
-        IschoolUser user = new IschoolUser();
-
-        return false;
-    }
-
-
-    @Override
-    public IschoolUser createUser(@Valid IschoolUser unStoreUser,
-                                  BindingResult errorResult) {
-
-
-        System.out.println(errorResult);
-
-        return null;
-    }
 }
