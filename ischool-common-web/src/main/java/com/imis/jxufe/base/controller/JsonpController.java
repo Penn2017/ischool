803452999feb1bd49449da3bc63584b68de16447
zhongpingtang <you@example.com>
2017-03-29 21:04:31 +0800

    测试jsonp

---
+++ ischool-common-web/src/main/java/com/imis/jxufe/base/controller/JsonpController.java
@@ -0,0 +1,15 @@
+package com.imis.jxufe.base.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+
+/**
+ * 处理jsonp的controller
+ * @author zhongping
+ * @date 2017/3/29
+ */
+@ControllerAdvice
+public class JsonpController extends AbstractJsonpResponseBodyAdvice {
+
+
+}
