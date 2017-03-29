d80481c4446893a5eac75d0dbbb0063684035982
zhongpingtang <you@example.com>
2017-03-29 18:56:08 +0800

    检验日志

--- ischool-common-web/src/main/java/com/imis/jxufe/base/utils/CallbackMappingJackson2HttpMessageConverter.java
+++ ischool-common-web/src/main/java/com/imis/jxufe/base/utils/CallbackMappingJackson2HttpMessageConverter.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -15,6 +17,8 @@ import java.io.IOException;
 
 public class CallbackMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
 
+	private Logger THIS_LOGGER = LoggerFactory.getLogger(CallbackMappingJackson2HttpMessageConverter.class);
+
 	// 做jsonp的支持的标识，在请求参数中加该参数
 	private String callbackName;
 
@@ -24,11 +28,13 @@ public class CallbackMappingJackson2HttpMessageConverter extends MappingJackson2
 		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
 		String callbackParam = request.getParameter(callbackName);
 		if(StringUtils.isEmpty(callbackParam)){
+			THIS_LOGGER.debug("=============没有json跨域问题===========================");
 			// 没有找到callback参数，直接返回json数据
 			super.writeInternal(object, outputMessage);
 		}else{
 			JsonEncoding encoding = getJsonEncoding(outputMessage.getHeaders().getContentType());
 			try {
+				THIS_LOGGER.debug("=============添加跨域支持==========================="+outputMessage.getBody());
 				String result =callbackParam+"("+super.getObjectMapper().writeValueAsString(object)+");";
 				IOUtils.write(result, outputMessage.getBody(),encoding.getJavaName());
 			}
