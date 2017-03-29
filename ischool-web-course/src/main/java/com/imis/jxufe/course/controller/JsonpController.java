b66e509bf60cf3a4e4ce4ccf79c7642684c3c008
zhongpingtang <you@example.com>
2017-03-29 21:46:47 +0800

    jsonp改造

---
+++ ischool-web-course/src/main/java/com/imis/jxufe/course/controller/JsonpController.java
@@ -0,0 +1,18 @@
+package com.imis.jxufe.course.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
+
+
+/**
+ * 处理jsonp的controller
+ * @author zhongping
+ * @date 2017/3/29
+ */
+@ControllerAdvice
+public class JsonpController extends AbstractJsonpResponseBodyAdvice {
+
+    public JsonpController() {
+        super("callback");
+    }
+}
