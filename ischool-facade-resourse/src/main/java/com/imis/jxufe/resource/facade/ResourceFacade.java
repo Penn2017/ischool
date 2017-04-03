f2db6d2f3be8048f6c14f1219af8781143e12668
zhongpingtang <you@example.com>
2017-04-03 21:47:52 +0800

    创建资源服务

---
+++ ischool-facade-resourse/src/main/java/com/imis/jxufe/resource/facade/ResourceFacade.java
@@ -0,0 +1,9 @@
+package com.imis.jxufe.resource.facade;
+
+/**
+ * 资源服务接口
+ * @author zhongping
+ * @date 2017/4/3
+ */
+public interface ResourceFacade {
+}
---
+++ ischool-service-resourse/src/main/java/com/imis/jxufe/resource/facade/impl/ResourceFacadeImpl.java
@@ -0,0 +1,12 @@
+package com.imis.jxufe.resource.facade.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.imis.jxufe.resource.facade.ResourceFacade;
+
+/**
+ * @author zhongping
+ * @date 2017/4/3
+ */
+@Service
+public class ResourceFacadeImpl implements ResourceFacade {
+}
